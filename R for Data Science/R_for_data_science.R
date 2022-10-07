1db72d47b912b52ed0b4a4cf9f21805707ddc5da
Hector Fugihara Kroes <hector.kroes@fm.usp.br>
2022-10-07 00:04:03 -0300

    tasks completed

--- R for Data Science/R_for_data_science.R
+++ R for Data Science/R_for_data_science.R
@@ -7,6 +7,12 @@ library(plotly)
 data <- read.csv('gapminder_clean.csv') %>%
   as_tibble()
 
+colnames(data)[colnames(data) == "CO2.emissions..metric.tons.per.capita."] <- "co2_emissions"
+colnames(data)[colnames(data) == "Population.density..people.per.sq..km.of.land.area."] <- "population_density"
+colnames(data)[colnames(data) == "Imports.of.goods.and.services....of.GDP."] <- "imports"
+colnames(data)[colnames(data) == "Energy.use..kg.of.oil.equivalent.per.capita."] <- "energy_use"
+colnames(data)[colnames(data) == "Life.expectancy.at.birth..total..years."] <- "life_expectancy"
+
 #Filter the data to include only rows where Year is 1962
 
 data_on_62 <- data %>% 
@@ -14,32 +20,32 @@ data_on_62 <- data %>%
 
 #Make a scatter plot comparing 'CO2 emissions (metric tons per capita)' and gdpPercap for the filtered data.
 
-ggplot(data_on_62, aes(x=CO2.emissions..metric.tons.per.capita., y = gdpPercap)) +
+ggplot(data_on_62, aes(x=co2_emissions, y = gdpPercap)) +
   geom_point()+
   labs(x="CO2 emissions (metric tons per capita)",y="GDP per capita",
        title="GDP per capita variation according to CO2 emissions")
   
 #Calculate the correlation of 'CO2 emissions (metric tons per capita)' and gdpPercap
 
-cor_test_res <- cor.test(data_on_62$CO2.emissions..metric.tons.per.capita., data_on_62$gdpPercap)
+cor_test_res <- cor.test(data_on_62$co2_emissions, data_on_62$gdpPercap)
 cor_test_res
 
 #In what year is the correlation between 'CO2 emissions (metric tons per capita)' and gdpPercap the strongest?
 
 highestcor <- data %>%
-  select(Country.Name,Year,gdpPercap,CO2.emissions..metric.tons.per.capita.) %>%
+  select(Country.Name,Year,gdpPercap,co2_emissions) %>%
   drop_na() %>%
   group_by(Year) %>% 
-  summarize(cor = cor(CO2.emissions..metric.tons.per.capita., gdpPercap)) %>%
+  summarize(cor = cor(co2_emissions, gdpPercap)) %>%
   top_n(1,cor)
 highestcor
 
 #Create an interactive scatter plot comparing 'CO2 emissions (metric tons per capita)' and gdpPercap, where the point size is determined by pop (population) and the color is determined by the continent
 
 co2_gdp_scatterplot <- data_on_62 %>%
-  select(Country.Name,Year,CO2.emissions..metric.tons.per.capita.,continent,pop,gdpPercap) %>%
+  select(Country.Name,Year,co2_emissions,continent,pop,gdpPercap) %>%
   drop_na() %>%
-  ggplot(aes(x=CO2.emissions..metric.tons.per.capita., 
+  ggplot(aes(x=co2_emissions, 
              y=gdpPercap,
              color=continent,
              size=pop)) + 
@@ -52,7 +58,7 @@ ggplotly(co2_gdp_scatterplot)
 
 #What is the relationship between continent and 'Energy use (kg of oil equivalent per capita)'?
 
-energy_continent_anova <- aov(Energy.use..kg.of.oil.equivalent.per.capita. ~ continent, data = data)
+energy_continent_anova <- aov(energy_use ~ continent, data = data)
 summary(energy_continent_anova)
 
 #Is there a significant difference between Europe and Asia with respect to 'Imports of goods and services (% of GDP)' in the years after 1990?
@@ -60,10 +66,10 @@ summary(energy_continent_anova)
 relevant_continents <- c("Europe","Asia")
 
 data_as_eu_after_90 <- data %>% 
-  select(Country.Name,Year,Imports.of.goods.and.services....of.GDP.,continent) %>%
+  select(Country.Name,Year,imports,continent) %>%
   filter(continent %in% relevant_continents, Year>1990)
 
-europe_asia_imports_t_test <- t.test(data_as_eu_after_90$Imports.of.goods.and.services....of.GDP.[data_as_eu_after_90$continent=='Europe'],data_as_eu_after_90$Imports.of.goods.and.services....of.GDP.[data_as_eu_after_90$continent=='Asia'])
+europe_asia_imports_t_test <- t.test(data_as_eu_after_90$imports[data_as_eu_after_90$continent=='Europe'],data_as_eu_after_90$Imports.of.goods.and.services....of.GDP.[data_as_eu_after_90$continent=='Asia'])
 europe_asia_imports_t_test
 
 #What is the country (or countries) that has the highest 'Population density (people per sq. km of land area)' across all years?
@@ -76,13 +82,12 @@ names(pop_density_ranking) <- countries
 
 for (x in years) {
   year_data <- data %>% 
-    select(Country.Name,Year,Population.density..people.per.sq..km.of.land.area.) %>%
+    select(Country.Name,Year,population_density) %>%
     na.omit()  %>%
     filter(Year == x)
-  year_data$Population.density..people.per.sq..km.of.land.area. <- rank(year_data$Population.density..people.per.sq..km.of.land.area.,na.last = NA)
-  rank(year_data$Population.density..people.per.sq..km.of.land.area.,na.last = FALSE)
+  year_data$population_density <- rank(year_data$population_density,na.last = TRUE)
   for (z in year_data$Country.Name) {
-    pop_density_ranking[[z]] <- pop_density_ranking[[z]] + year_data$Population.density..people.per.sq..km.of.land.area.[year_data$Country.Name==z]
+    pop_density_ranking[[z]] <- pop_density_ranking[[z]] + year_data$population_density[year_data$Country.Name==z]
   }
 }
 
@@ -90,12 +95,32 @@ pop_density_ranking <- pop_density_ranking %>%
   sort(decreasing = TRUE) %>%
   replace(pop_density_ranking==0, NA)
 
-ggplot(data, aes(x = Year, y = log10(Population.density..people.per.sq..km.of.land.area.), group = Country.Name, color = Country.Name, label = Country.Name)) +
+pop_density_ranking
+
+ggplot(data, aes(x = Year, y = log10(population_density), group = Country.Name, color = Country.Name, label = Country.Name)) +
   geom_line() +
   theme(legend.position = "none")+
-  geom_text(data = subset(data, ((Year==2002 & Population.density..people.per.sq..km.of.land.area.>780)|(Year==2002 & Population.density..people.per.sq..km.of.land.area.<10))), 
+  geom_text(data = subset(data, ((Year==2002 & population_density>780)|(Year==2002 & population_density<10))), 
     aes(label = Country.Name, color = Country.Name, hjust = 0, vjust = 1))+
   labs(y="Population density (log10(people/square km of land area))",x="Year",
     title="Population density variation according to year per country",)
 
 #What country (or countries) has shown the greatest increase in 'Life expectancy at birth, total (years)' since 1962?
+
+life_expectancy_diff <- rep(0, times=length(countries))
+names(life_expectancy_diff) <- countries
+
+for (z in countries) {
+  country_life_exp <- data %>% 
+    select(Country.Name,Year,life_expectancy) %>%
+    filter(Country.Name==z)
+  years <- unique(country_life_exp$Year)
+  print(country_life_exp$life_expectancy[country_life_exp$Year==tail(years,1)])
+  print(country_life_exp$life_expectancy[country_life_exp$Year==head(years,1)])
+  life_expectancy_diff[z] <- country_life_exp$life_expectancy[country_life_exp$Year==tail(years,1)] - country_life_exp$life_expectancy[country_life_exp$Year==head(years,1)] 
+}
+
+life_expectancy_diff <- life_expectancy_diff %>%
+  sort(decreasing = TRUE)
+
+life_expectancy_diff
