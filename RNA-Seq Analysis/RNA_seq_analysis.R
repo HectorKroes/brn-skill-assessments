06b0063c19566c78404b931ca62dd940f4b4a922
Hector Fugihara Kroes <hector.kroes@fm.usp.br>
2022-10-13 23:24:15 -0300

    heatmap and gsea done

--- RNA-Seq Analysis/RNA_seq_analysis.R
+++ RNA-Seq Analysis/RNA_seq_analysis.R
@@ -4,12 +4,12 @@ library(ggplot2)
 library(gplots)
 library(tidyverse)
 library(ggrepel)
-library(org.Hs.eg.db)
-library(clusterProfiler)
 library(DOSE)
 library(DT)
 require(biomaRt)
 library(pheatmap)
+library(fgsea)
+library(reactome.db)
 
 study_acession <- "SRP041538"
 
@@ -29,14 +29,15 @@ dds$treatment <- relevel(dds$condition, ref = "NOR")
 dds <- DESeq(dds)
 
 res <- as_tibble(results(dds, alpha = 0.05), rownames = "gene") %>%
-  rename(logFC=log2FoldChange) %>% rename(FDR=padj)
+  rename(logFC = log2FoldChange) %>%
+  rename(FDR = padj)
 
 ensembl <- useMart("ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host = "www.ensembl.org")
 
 genes <- gsub("\\..*", "", res$gene)
 
 genemap <- getBM(
-  attributes = c("ensembl_gene_id", "hgnc_symbol"),
+  attributes = c("ensembl_gene_id", "hgnc_symbol", "entrezgene_id"),
   filters = "ensembl_gene_id",
   values = genes,
   mart = ensembl
@@ -56,7 +57,7 @@ check_local_symbol <- function(ref, local_symbols) {
 }
 
 local_symbols <- data.frame(rowData(dds)$symbol)
-local_symbols$value[is.na(local_symbols$value)]<-""
+local_symbols$value[is.na(local_symbols$value)] <- ""
 gene_id_list <- NULL
 
 
@@ -66,25 +67,25 @@ for (ref in genes) {
     if (!hgnc_symbol == "") {
       gene_id_list <- append(gene_id_list, hgnc_symbol)
     } else {
-      ref<-check_local_symbol(ref, local_symbols)
+      ref <- check_local_symbol(ref, local_symbols)
       gene_id_list <- append(gene_id_list, ref)
     }
   } else {
-    ref<-check_local_symbol(ref, local_symbols)
+    ref <- check_local_symbol(ref, local_symbols)
     gene_id_list <- append(gene_id_list, ref)
   }
 }
 
-res<-res %>%
-  add_column(symbol=gene_id_list) %>%
+res <- res %>%
+  add_column(symbol = gene_id_list) %>%
   relocate(symbol, .after = gene)
 
 deseq_genes <- res %>%
   as.data.frame() %>%
   filter(FDR < 0.05) %>%
-  filter(logFC>=log2(2)|logFC<=-log2(2)) %>%
-  dplyr::select(EnsemblID= gene, symbol, logFC, pvalue, FDR) %>%
-arrange(FDR)
+  filter(logFC >= log2(2) | logFC <= -log2(2)) %>%
+  dplyr::select(EnsemblID = gene, symbol, logFC, pvalue, FDR) %>%
+  arrange(FDR)
 deseq_genes_count <- nrow(deseq_genes)
 output_note <- paste0("\nTotal differential expressed genes with adjusted p<0.05: ", deseq_genes_count, "\n(top 1000 shown above)")
 deseq_genes %>%
@@ -108,17 +109,21 @@ res <- res %>%
   )
 
 
-top <- 25
-top_genes <- bind_rows(
-  res %>%
-    filter(Expression == "Up-regulated") %>%
-    arrange(FDR, desc(abs(logFC))) %>%
-    head(top),
-  res %>%
-    filter(Expression == "Down-regulated") %>%
-    arrange(FDR, desc(abs(logFC))) %>%
-    head(top)
-)
+top_bottom <- function(top, res) {
+  top_genes <- bind_rows(
+    res %>%
+      filter(Expression == "Up-regulated") %>%
+      arrange(FDR, desc(abs(logFC))) %>%
+      head(top),
+    res %>%
+      filter(Expression == "Down-regulated") %>%
+      arrange(FDR, desc(abs(logFC))) %>%
+      head(top)
+  )
+  return(top_genes)
+}
+
+top_genes
 
 volcano_plot <- ggplot(res, aes(logFC, -log(FDR, 10))) +
   geom_point(aes(color = Significance), size = 2 / 5) +
@@ -130,7 +135,7 @@ volcano_plot <- ggplot(res, aes(logFC, -log(FDR, 10))) +
   geom_hline(yintercept = -log10(0.05), col = "red") +
   ggtitle("Lung tissue transcriptome of COPD patients x Healthy individuals") +
   geom_label_repel(
-    data = top_genes,
+    data = top_bottom(25, res),
     mapping = aes(logFC, -log(FDR, 10), label = symbol),
     size = 2,
     max.overlaps = Inf
@@ -138,52 +143,90 @@ volcano_plot <- ggplot(res, aes(logFC, -log(FDR, 10))) +
 
 volcano_plot
 
-#PCA
+# PCA
 
-tdds <- vst(dds,blind = T)
+tdds <- vst(dds, blind = T)
 
-pcaData <- plotPCA(tdds, intgroup="condition", returnData=TRUE)
+pcaData <- plotPCA(tdds, intgroup = "condition", returnData = TRUE)
 
 percentVar <- round(100 * attr(pcaData, "percentVar"))
 
-ggplot(pcaData, aes(PC1, PC2, color=group)) +
-  geom_point(size=3) +
-  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
-  ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
-  coord_fixed()+
-  stat_ellipse(aes(x=PC1,y=PC2,fill=factor(group)), show.legend=FALSE,
-               geom="polygon", level=0.8, alpha=0.05)+
-  guides(color=guide_legend("Cluster"))+
+ggplot(pcaData, aes(PC1, PC2, color = group)) +
+  geom_point(size = 3) +
+  xlab(paste0("PC1: ", percentVar[1], "% variance")) +
+  ylab(paste0("PC2: ", percentVar[2], "% variance")) +
+  coord_fixed() +
+  stat_ellipse(aes(x = PC1, y = PC2, fill = factor(group)),
+    show.legend = FALSE,
+    geom = "polygon", level = 0.8, alpha = 0.05
+  ) +
+  guides(color = guide_legend("Cluster")) +
   ggtitle("PCA plot of COPD patients x Healthy individuals")
 
-#Heatmap
+# Heatmap
 
 tddsa <- assay(tdds)
 rownames(tddsa) <- gsub("\\..*", "", row.names(tddsa))
-topDE <- tddsa[genes,]
-rownames(topDE) <- res$symbol
-top_bottom_DE <- rbind(head(topDE, n=10), tail(topDE, n=10))
-annotation <- as.data.frame(colData(tdds)[, 'condition'])
-rownames(annotation) <- colnames(top_bottom_DE)
-pheatmap(top_bottom_DE, scale = "row", clustering_distance_rows = "correlation", annotation_col = annotation, main="Top 10 over-expressed \nand top 10 under-expressed \nDifferentially Expressed genes")
 
-#GSE
+heat_genes <- top_bottom(10, res)
+heat_genes$gene <- gsub("\\..*", "", heat_genes$gene)
 
-human <- "org.Hs.eg.db"
+topDE <- tddsa[heat_genes$gene, ]
+rownames(topDE) <- heat_genes$symbol
 
-stat_list<-res$stat
-names(stat_list)<-genes
-gene_list = sort(stat_list, decreasing = TRUE)
+col_annotation <- as.data.frame(colData(tdds)[, "condition"])
+colnames(col_annotation) <- "Condition"
+rownames(col_annotation) <- colnames(topDE)
 
-gse <- gseGO(geneList=gene_list, 
-             ont ="MF", 
-             keyType = "ENSEMBL", 
-             nPerm = 10000, 
-             minGSSize = 3, 
-             maxGSSize = 800, 
-             pvalueCutoff = 0.05, 
-             verbose = TRUE, 
-             OrgDb = human, 
-             pAdjustMethod = "none")
+row_annotation <- as.data.frame(heat_genes$Expression)
+colnames(row_annotation) <- "Expression"
+rownames(row_annotation) <- heat_genes$symbol
 
-dotplot(gse, showCategory=10, split=".sign", font.size = 5) + facet_grid(.~.sign)
+pheatmap(topDE,
+  scale = "row",
+  clustering_distance_rows = "correlation",
+  annotation_col = col_annotation,
+  annotation_row = row_annotation,
+  show_colnames = FALSE,
+  annotation_names_row = FALSE,
+  annotation_names_col = FALSE,
+  main = "20 genes with the strongest differential expression"
+)
+
+# GSEA
+
+genemap$entrezgene_id[is.null(genemap$entrezgene_id)] <- NA
+
+entrez_id_list <- NULL
+
+for (ref in genes) {
+  if (ref %in% genemap$ensembl_gene_id) {
+    entrez_id <- genemap$entrezgene_id[genemap$ensembl_gene_id == ref][1]
+    if (!is.na(entrez_id)) {
+      entrez_id_list <- append(entrez_id_list, entrez_id)
+    } else {
+      entrez_id_list <- append(entrez_id_list, NA)
+    }
+  } else {
+    entrez_id_list <- append(entrez_id_list, NA)
+  }
+}
+
+res$entrez_id <- as.character(entrez_id_list)
+
+gsea_data <- subset(res, !is.na(stat)) %>%
+  subset(!is.na(FDR))
+
+pathways <- reactomePathways(gsea_data$entrez_id)
+
+ranked_genes <- gsea_data$stat
+names(ranked_genes) <- gsea_data$entrez_id
+
+fgseaRes <- fgsea(pathways, ranked_genes, maxSize = 500)
+
+topPathwaysUp <- fgseaRes[NES > 0][head(order(padj), n = 10), pathway]
+topPathwaysDown <- fgseaRes[NES < 0][head(order(padj), n = 10), pathway]
+topPathways <- c(topPathwaysUp, rev(topPathwaysDown))
+plotGseaTable(pathways[topPathways], ranked_genes, fgseaRes,
+  gseaParam = 0.5
+)
